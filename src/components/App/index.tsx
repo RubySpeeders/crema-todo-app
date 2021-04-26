057521c82d2fe3cfef7091ed3ec2fe5ee03aeceb
RubySpeeders <sarahnpeters@gmail.com>
2021-04-26 11:55:57 -0500

    ADASP-16: downloaded new scripts. commented out tests in cypress. added sections of new tasks and copmleted tasks

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,23 +1,35 @@
-import logo from "../../assets/logo.svg"
+// import logo from "../../assets/logo.svg"
 import "./styles.css"
 
 export function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/components/App/index.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>My Tasks</h1>
       </header>
+      <body>
+        <h3>Active Tasks</h3>
+        <form>
+          <label>New Task</label>
+          <input></input>
+          <button>Cancel</button>
+          <button>Save</button>
+        </form>
+        <section className="Task-list">
+          <label>
+            <input name="task" type="checkbox" />
+            Complete development of apprenticeship practice to-do app
+          </label>
+        </section>
+        <button>Create New Task</button>
+        <section className="Complete-list">
+          <h3>Completed Tasks</h3>
+          <label>
+            <input name="task" type="checkbox" />
+            Lorem ipsum
+          </label>
+        </section>
+      </body>
     </div>
   )
 }
