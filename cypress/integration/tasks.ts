6b5567304affce9f086a4c45e917de348c2f03b2
RubySpeeders <sarahnpeters@gmail.com>
2021-04-28 14:40:13 -0500

    ADASP-22: fix cypress test

--- cypress/integration/tasks.ts
+++ cypress/integration/tasks.ts
@@ -1,7 +1,7 @@
 describe("App", function () {
   it("Contains a link to learn about React", function () {
     cy.visit("/")
-    cy.get(".App-header").contains("My Tasks")
+    cy.get(".AppHeader").contains("My Tasks")
     cy.get('[type="checkbox"]').should("exist")
     // cy.findByTestId("kittykat")
   })
