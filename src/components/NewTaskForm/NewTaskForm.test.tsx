02f4be2d9cd6bdb6536b1667beefa64b76a4aa77
RubySpeeders <sarahnpeters@gmail.com>
2021-04-28 14:36:12 -0500

    ADASP-22: remove getLabelText from NewTaskForm tests

--- src/components/NewTaskForm/NewTaskForm.test.tsx
+++ src/components/NewTaskForm/NewTaskForm.test.tsx
@@ -1,4 +1,4 @@
-import { getByLabelText, render } from "@testing-library/react"
+import { render } from "@testing-library/react"
 // import React from "react"
 import { NewTaskForm } from "./NewTaskForm"
 
