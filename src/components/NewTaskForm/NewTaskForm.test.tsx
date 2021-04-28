8b164f29fc7bf57904a2fca80c601dc82d29a0aa
RubySpeeders <sarahnpeters@gmail.com>
2021-04-28 11:27:19 -0500

    ADASP-22: removed React import from .test files

--- src/components/NewTaskForm/NewTaskForm.test.tsx
+++ src/components/NewTaskForm/NewTaskForm.test.tsx
@@ -1,5 +1,5 @@
 import { render } from "@testing-library/react"
-import React from "react"
+// import React from "react"
 import { NewTaskForm } from "./NewTaskForm"
 
 describe("NewTaskForm", () => {
