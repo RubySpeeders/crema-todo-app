393f7bffdcc34a0374113b1fe335ddd9b9f2014f
RubySpeeders <sarahnpeters@gmail.com>
2021-04-29 15:30:06 -0500

    ADASP-22: fixing tests in ButtonCreateTask

--- src/components/ButtonCreateTask/ButtonCreateTask.test.tsx
+++ src/components/ButtonCreateTask/ButtonCreateTask.test.tsx
@@ -1,14 +1,14 @@
 import { render } from "@testing-library/react"
 // import React from "react"
-import { CreateTaskButton } from "./ButtonCreateTask"
+import { ButtonCreateTask } from "./ButtonCreateTask"
 
-describe("CreateTaskButton", () => {
+describe("ButtonCreateTask", () => {
   it("displays button text", () => {
     // Arrange
     const name = "Create New Task"
 
     // Act
-    const { getByText } = render(<CreateTaskButton />)
+    const { getByText } = render(<ButtonCreateTask />)
     const received = getByText(name)
 
     // Assert
