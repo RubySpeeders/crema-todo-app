e2fbf9cacead41e7317a335f85d4478b28dacb27
Sarah Peters <69406113+RubySpeeders@users.noreply.github.com>
2021-05-03 15:40:50 -0500

    ADASP-24: adding cypress test for putting text in input and clicking save button

--- cypress/integration/tasks.ts
+++ cypress/integration/tasks.ts
@@ -2,7 +2,8 @@ describe("App", function () {
   it("Contains a link to learn about React", function () {
     cy.visit("/")
     cy.get(".AppHeader").contains("My Tasks")
-    //need to learn about how to add a task and then check for this
-    // cy.get('[type="checkbox"]').should("exist")
+    cy.get("#taskInput").type("make dinner")
+    cy.get("#saveButton").click()
+    cy.get('[type="checkbox"]').should("exist")
   })
 })
