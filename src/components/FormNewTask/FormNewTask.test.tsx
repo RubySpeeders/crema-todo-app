796d08d35f2c52c0b692ace3e1277ac2ae0aa470
Sarah Peters <69406113+RubySpeeders@users.noreply.github.com>
2021-05-18 13:48:01 -0500

    ADASP-28: adding a section for edit task testing

--- src/components/FormNewTask/FormNewTask.test.tsx
+++ src/components/FormNewTask/FormNewTask.test.tsx
@@ -26,4 +26,29 @@ describe("FormNewTask", () => {
     // Assert
     expect(onAddTask).toHaveBeenCalledWith("meeting with Mandy")
   })
+  //TODO: CHANGE TO EDIT TASK PARAMETERS INSTEAD OF ADD TASK PARAMETERS
+  it("tests onEditTask is called with correct parameter", () => {
+    // Arrange
+    const onAddTask = jest.fn()
+    const modal = true
+
+    // Act
+    const { getByLabelText, getByText } = render(
+      <FormNewTask
+        label="New Task"
+        placeholder="TaskDescription"
+        onAddTask={onAddTask}
+        modal={modal}
+        onHideModal={onAddTask}
+      />,
+    )
+
+    const input = getByLabelText("New Task", { selector: "input" })
+    const saveButton = getByText("Save")
+    fireEvent.change(input, { target: { value: "meeting with Mandy" } })
+    fireEvent.click(saveButton)
+
+    // Assert
+    expect(onAddTask).toHaveBeenCalledWith("meeting with Mandy")
+  })
 })
