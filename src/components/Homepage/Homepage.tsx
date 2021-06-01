5ddbe0f6d867f6be459680a57e8097234943a0b5
Sarah Peters <69406113+RubySpeeders@users.noreply.github.com>
2021-06-01 09:47:14 -0500

    ADASP-23: styles and parent/child for purple icon

--- src/components/Homepage/Homepage.tsx
+++ src/components/Homepage/Homepage.tsx
@@ -6,8 +6,12 @@ export function Homepage() {
   return (
     <div className="homepage">
       <div className="homepageContent">
-        <div className="circle"></div>
-        <CheckIcon />
+        <div className="circle">
+          <div className="child">
+            <CheckIcon />
+            <p>todo</p>
+          </div>
+        </div>
         <div className="welcome">
           <h5>Welcome!</h5>
           <p>Get things done with Todo</p>
