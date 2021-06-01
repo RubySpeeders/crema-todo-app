386cb8f653086e350779e715d7e3e89f6c9f679e
Sarah Peters <69406113+RubySpeeders@users.noreply.github.com>
2021-06-01 17:12:29 -0500

    ADASP-23: adding BrowserRouter to Homepage tests

--- src/components/Homepage/Homepage.test.tsx
+++ src/components/Homepage/Homepage.test.tsx
@@ -1,11 +1,17 @@
 import ReactDOM from "react-dom"
+import { BrowserRouter } from "react-router-dom"
 import { Homepage } from "./Homepage"
 
 //TODO clean up, make real tests
 describe("Homepage", () => {
   it("renders", () => {
     const div = document.createElement("div")
-    ReactDOM.render(<Homepage />, div)
+    ReactDOM.render(
+      <BrowserRouter>
+        <Homepage />
+      </BrowserRouter>,
+      div,
+    )
     ReactDOM.unmountComponentAtNode(div)
   })
 })
