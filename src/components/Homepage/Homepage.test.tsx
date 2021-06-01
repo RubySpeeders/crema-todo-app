4abf57f68d34a647549906c1214e05a9d9a87adf
Sarah Peters <69406113+RubySpeeders@users.noreply.github.com>
2021-06-01 17:10:00 -0500

    ADASP-23: adding in a temporary test on Homepage to see if it runs

--- src/components/Homepage/Homepage.test.tsx
+++ src/components/Homepage/Homepage.test.tsx
@@ -1,16 +1,11 @@
-import { render } from "@testing-library/react"
+import ReactDOM from "react-dom"
 import { Homepage } from "./Homepage"
 
+//TODO clean up, make real tests
 describe("Homepage", () => {
-  it("name prop is rendered", () => {
-    // Arrange
-    const name = "Homepage"
-
-    // Act
-    const { getByText } = render(<Homepage />)
-    const received = getByText(name)
-
-    // Assert
-    expect(received).toBeDefined()
+  it("renders", () => {
+    const div = document.createElement("div")
+    ReactDOM.render(<Homepage />, div)
+    ReactDOM.unmountComponentAtNode(div)
   })
 })
