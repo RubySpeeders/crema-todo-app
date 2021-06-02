c88898a7fa7743999b36738f6dac0a3076ccf961
Sarah Peters <69406113+RubySpeeders@users.noreply.github.com>
2021-06-02 14:28:45 -0500

    ADASP-31: adding styling to button link and fixing merge mess

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,71 +1,5 @@
-<<<<<<< HEAD
-import ExpandLessIcon from "@material-ui/icons/ExpandLess"
-import ExpandMoreIcon from "@material-ui/icons/ExpandMore"
-import { useState } from "react"
-import { useAppSelector } from "../../redux/hooks"
-import "./styles.css"
-import { Task } from "../../types/Task"
-import { AppHeader } from "../AppHeader"
-import { ButtonCreateTask } from "../ButtonCreateTask"
-import { FormNewTask } from "../FormNewTask"
-import { TaskCard } from "../TaskCard"
-
-export function App() {
-  const tasks: Task[] = useAppSelector((state) => state.allTasks.tasks)
-  const [modal, setModal] = useState(false)
-  const [expanded, setExpand] = useState(true)
-  const completedTasks = tasks.filter((task) => task.isComplete)
-  const activeTasks = tasks.filter((task) => !task.isComplete)
-
-  const handleModal = () => {
-    setModal(!modal)
-  }
-
-  const handleExpand = () => {
-    setExpand(!expanded)
-  }
-
-  return (
-    <div className="App">
-      <AppHeader />
-      <main>
-        <div className="activeList">
-          <p>Active Tasks</p>
-          {activeTasks.map((taskItem: Task) => {
-            return <TaskCard key={taskItem.id} task={taskItem} />
-          })}
-        </div>
-        <ButtonCreateTask onShowModal={handleModal} />
-        {modal}
-        <div className="completeList">
-          <div className="completeLabel">
-            <p className="complete">Completed Tasks</p>
-            {expanded ? (
-              <div className="expand active" onClick={handleExpand}>
-                Hide
-                <ExpandLessIcon />
-              </div>
-            ) : (
-              <div className="expand" onClick={handleExpand}>
-                Show
-                <ExpandMoreIcon />
-              </div>
-            )}
-          </div>
-          {expanded &&
-            completedTasks.map((taskItem: Task) => {
-              return <TaskCard key={taskItem.id} task={taskItem} />
-            })}
-        </div>
-      </main>
-      <FormNewTask
-        onHideModal={handleModal}
-        kind="new"
-        modal={modal}
-        placeholder="Task description"
-      />
-=======
 import { BrowserRouter, Route } from "react-router-dom"
+import "./styles.css"
 import { Homepage } from "../Homepage"
 import { TaskPage } from "../TaskPage"
 
@@ -76,7 +10,6 @@ export function App() {
         <Route exact path="/" component={Homepage} />
         <Route exact path="/todo" component={TaskPage} />
       </BrowserRouter>
->>>>>>> develop
     </div>
   )
 }
--- src/components/TaskPage/TaskPage.tsx
+++ src/components/TaskPage/TaskPage.tsx
@@ -1,8 +1,8 @@
 import ExpandLessIcon from "@material-ui/icons/ExpandLess"
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore"
-import { nanoid } from "nanoid"
 import { useState } from "react"
 import "./style.css"
+import { useAppSelector } from "../../redux/hooks"
 import { Task } from "../../types/Task"
 import { AppHeader } from "../AppHeader"
 import { ButtonCreateTask } from "../ButtonCreateTask"
@@ -10,59 +10,11 @@ import { FormNewTask } from "../FormNewTask"
 import { TaskCard } from "../TaskCard"
 
 export function TaskPage() {
+  const tasks: Task[] = useAppSelector((state) => state.allTasks.tasks)
   const [modal, setModal] = useState(false)
-  const [tasks, setTasks] = useState<Task[]>([])
   const [expanded, setExpand] = useState(true)
   const completedTasks = tasks.filter((task) => task.isComplete)
   const activeTasks = tasks.filter((task) => !task.isComplete)
-
-  const addTask = (text: string) => {
-    setTasks((previousArray) => [
-      ...previousArray,
-      { id: nanoid(), description: text, isComplete: false },
-    ])
-  }
-
-  const statusHandler = (id: string) => {
-    setTasks((previousArray) => {
-      const found = previousArray.find((task) => task.id === id)
-      if (found) {
-        return [
-          ...previousArray.filter((item) => item.id !== id),
-          { ...found, isComplete: !found.isComplete },
-        ]
-      } else {
-        return [...previousArray]
-      }
-    })
-  }
-
-  const editTask = (id: string, text: string) => {
-    setTasks((previousArray) => {
-      const found = previousArray.find((task) => task.id === id)
-      if (found) {
-        return previousArray.map((item) => {
-          if (item === found) {
-            return { ...found, description: text }
-          }
-          return item
-        })
-      } else {
-        return [...previousArray]
-      }
-    })
-  }
-
-  const deleteTask = (id: string) => {
-    setTasks((previousArray) => {
-      const found = previousArray.find((task) => task.id === id)
-      if (found) {
-        return [...previousArray.filter((item) => item.id !== id)]
-      } else {
-        return [...previousArray]
-      }
-    })
-  }
 
   const handleModal = () => {
     setModal(!modal)
@@ -84,15 +36,7 @@ export function TaskPage() {
           <div className="activeList">
             <p>Active Tasks</p>
             {activeTasks.map((taskItem: Task) => {
-              return (
-                <TaskCard
-                  key={taskItem.id}
-                  task={taskItem}
-                  onStatusChange={statusHandler}
-                  onDeleteTask={deleteTask}
-                  onEditTask={editTask}
-                />
-              )
+              return <TaskCard key={taskItem.id} task={taskItem} />
             })}
           </div>
         )}
@@ -116,15 +60,7 @@ export function TaskPage() {
           </div>
           {expanded &&
             completedTasks.map((taskItem: Task) => {
-              return (
-                <TaskCard
-                  key={taskItem.id}
-                  task={taskItem}
-                  onStatusChange={statusHandler}
-                  onDeleteTask={deleteTask}
-                  onEditTask={editTask}
-                />
-              )
+              return <TaskCard key={taskItem.id} task={taskItem} />
             })}
         </div>
       </main>
@@ -132,7 +68,6 @@ export function TaskPage() {
         kind="new"
         modal={modal}
         onHideModal={handleModal}
-        onAddTask={addTask}
         placeholder="Task description"
       />
     </div>
