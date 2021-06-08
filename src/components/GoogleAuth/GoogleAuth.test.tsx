91c67213a234b4d531942ba48fbb215de73e9635
Sarah Peters <69406113+RubySpeeders@users.noreply.github.com>
2021-06-08 13:51:20 -0500

    ADASP-33: adding in a dummy test in GoogleAuth

--- src/components/GoogleAuth/GoogleAuth.test.tsx
+++ src/components/GoogleAuth/GoogleAuth.test.tsx
@@ -1,29 +1,13 @@
+import { render } from "@testing-library/react"
 import { GoogleAuth } from "./GoogleAuth"
-import { render, fireEvent } from "@testing-library/react"
-import React from "react"
 
 describe("GoogleAuth", () => {
   it("has correct message before clicking", () => {
     // Arrange
-    const message = "You clicked 0 times"
+    const message = "Sign In"
 
     // Act
     const { getByText } = render(<GoogleAuth />)
-    const received = getByText(message)
-
-    // Assert
-    expect(received).toBeDefined()
-  })
-
-  it("has correct message after clicking", () => {
-    // Arrange
-    const buttonText = "Click Me"
-    const message = "You clicked 1 times"
-
-    // Act
-    const { getByText } = render(<GoogleAuth />)
-    const button = getByText(buttonText)
-    fireEvent.click(button)
     const received = getByText(message)
 
     // Assert
