43f157a6548e917e8c8a957fd7dc166fb7efe81e
Sarah Peters <69406113+RubySpeeders@users.noreply.github.com>
2021-06-09 16:38:32 -0500

    ADASP-26: styles for width in the task list

--- src/components/ButtonCreateTask/ButtonCreateTask.tsx
+++ src/components/ButtonCreateTask/ButtonCreateTask.tsx
@@ -1,18 +1,19 @@
 import { useAppSelector } from "../../redux/hooks"
 import { Task } from "../../types/Task"
+import "./style.css"
 
 type Props = { onShowModal: () => void }
 
 export function ButtonCreateTask({ onShowModal }: Props) {
   const tasks: Task[] = useAppSelector((state) => state.allTasks.tasks)
   return (
-    <div>
+    <div className="buttonContainer">
       <button
         data-testid="createButton"
         className={tasks.length === 0 ? "primaryButton" : "createTaskButton"}
         onClick={onShowModal}
       >
-        <p id="newTaskText">Create New Task</p>
+        Create New Task
       </button>
     </div>
   )
