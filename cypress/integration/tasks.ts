7c700e578e35a50fe7665c968557d3695fd6a0f7
Sarah Peters <69406113+RubySpeeders@users.noreply.github.com>
2021-06-16 11:44:58 -0400

    ADASP-26: updating cypress tests to use cypress testing library

--- cypress/integration/tasks.ts
+++ cypress/integration/tasks.ts
@@ -1,8 +1,8 @@
 describe("App", function () {
   it("Contains a button that starts the app in motion", function () {
     cy.visit("/")
-    cy.get("[data-testid=getStarted]").contains("Get Started")
-    cy.get("[data-testid=getStarted]").click()
+    cy.findByText("Get Started").contains("Get Started")
+    cy.findByText("Get Started").click()
     cy.get(".AppHeader").contains("My Tasks")
     cy.url().should("include", "/todo")
   })
@@ -15,32 +15,32 @@ describe("App", function () {
     cy.findByText("Create New Task").click()
     cy.get(".taskForm").should("exist")
     cy.get("label").should("have.text", "New Task")
-    cy.get("[data-testid=cancel]").click()
+    cy.findByText("Cancel").click()
     cy.get(".taskForm").should("not.exist")
   })
   it("creates a new task with a checkbox", function () {
     cy.visit("/todo")
-    cy.get("[data-testid=createButton]").click()
+    cy.findByText("Create New Task").click()
     cy.get("#taskInput").type("make dinner")
-    cy.get("[data-testid=save]").click()
+    cy.findByText("Save").click()
     cy.get(".checkbox > .MuiSvgIcon-root").should("exist")
   })
   it("changes an active task to completed when checkbox is clicked", function () {
     cy.visit("/todo")
-    cy.get("[data-testid=createButton]").click()
+    cy.findByText("Create New Task").click()
     cy.get("#taskInput").type("make dinner")
-    cy.get("[data-testid=save]").click()
+    cy.findByText("Save").click()
     cy.get("[data-testid=unchecked]").click()
     cy.get("[data-testid=checked] > path").should("exist")
   })
   it("shows an edit modal after clicking a task, edits the description, then hides the modal after clicking save", function () {
     cy.visit("/todo")
-    cy.get("[data-testid=createButton]").click()
+    cy.findByText("Create New Task").click()
     cy.get("#taskInput").type("make dinner")
-    cy.get("[data-testid=save]").click()
+    cy.findByText("Save").click()
     cy.get(".taskCard").click()
     cy.get("#taskInput").type("wash the dishes")
-    cy.get("[data-testid=save]").click()
+    cy.findByText("Save").click()
     cy.get(".checkboxAndDescription > .normal").should(
       "have.text",
       "wash the dishes",
@@ -48,9 +48,9 @@ describe("App", function () {
   })
   it("shows and hides complete tasks", function () {
     cy.visit("/todo")
-    cy.get("[data-testid=createButton]").click()
+    cy.findByText("Create New Task").click()
     cy.get("#taskInput").type("another task")
-    cy.get("[data-testid=save]").click()
+    cy.findByText("Save").click()
     cy.get("[data-testid=unchecked]").click({ multiple: true })
     cy.get(".taskCard").should("exist")
     cy.get(".expand").click()
